bfb1578ab9fc7c16fb1c407937fb7de64a26169d
UrielShlang <uri.shlang@gmail.com>
2024-12-11 15:59:10 +0200

    .

--- detectCitrix.ps1
+++ detectCitrix.ps1
@@ -1,4 +1,9 @@
-$CitrixVersion = (Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -like "*Citrix Workspace 2402*" }).DisplayVersion
+# Fetch the version of Citrix installed
+$CitrixVersion = (Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" `
+| Where-Object { $_.DisplayName -like "*Citrix*" } `
+| Select-Object -ExpandProperty DisplayVersion -ErrorAction SilentlyContinue)
+
+# Check if the version matches
 if ($CitrixVersion -eq "24.2.1000.1016") {
     Exit 0  # Installed
 } else {
