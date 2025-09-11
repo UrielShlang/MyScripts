81029fd66afdf19834a5ca7497260d724e96047d
UrielShlang <uri.shlang@gmail.com>
2025-09-11 15:28:10 +0300

    .

--- SCRIPTS/EntraID_Exchange/1.ps1
+++ SCRIPTS/EntraID_Exchange/1.ps1
@@ -1,6 +1,28 @@
 Function TextMessage  {
  [CmdletBinding()]
-
+ Param
+ (
+     # My Twilio Number
+     [Parameter(Mandatory=$false)]
+     [ValidateNotNullOrEmpty()]
+     [string]$Twilio_NUMBER='+18147184353',
+     # My Test Number
+     [Parameter(Mandatory=$false)]
+     [ValidateNotNullOrEmpty()]
+     [string]$User_NUMBER='',
+     # Twilio Auth SID
+     [Parameter(Mandatory=$false)]
+     [ValidateNotNullOrEmpty()]
+     [string]$sid = "",
+     # Twilio Auth Token
+     [Parameter(Mandatory=$false)]
+     [ValidateNotNullOrEmpty()]
+     [string]$token = "",
+     # Twilio API endpoint and POST params
+     [Parameter(Mandatory=$false)]
+     [ValidateNotNullOrEmpty()]
+     [string]$url = "https://api.twilio.com/2010-04-01/Accounts/$sid/Messages.json"
+ )
 
 $body = @{
      From = $Twilio_NUMBER;
